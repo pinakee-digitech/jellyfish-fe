681a21a0b675391372b73c9231f491f251c2f2e8
arjun r <arjun.rdell@gmail.com>
2024-10-29 21:49:18 +0530

    created AuthContext to validate user

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import './App.css';
 import LoginSignupPage from './Pages/LoginSignupPage';
+import Home from './Pages/Home';
+import { AuthContext } from './Context/AuthContext';
+import { useEffect, useState } from 'react';
+import Services from './Shared/Services';
 
 function App() {
 
   // changes
 
   //test2
-  
+
   // changes1
 
   //ch
 
   // test pushing 
-  
+
+  const[userData, setUserData] = useState<any>(null)
+  useEffect(()=>{
+    Services.getUserAuth().then((res:any)=>{
+      console.log(res);
+      res?setUserData(res):setUserData(null);
+    })
+  },[])
+
 
   return (
     <div className='defaultFont'>
-      <LoginSignupPage></LoginSignupPage>
+      <AuthContext.Provider value={{userData, setUserData}}>
+        {userData?<Home/>:<LoginSignupPage/>}
+      </AuthContext.Provider>
     </div>
   )
 }
--- src/Pages/LoginSignupPage.tsx
+++ src/Pages/LoginSignupPage.tsx
@@ -1,13 +1,31 @@
 import Box from '@mui/material/Box';
+import '../App.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import Services from '../Shared/Services';
+import { AuthContext } from '../Context/AuthContext';
 
 export default function LoginSignupPage() {
     const[login, setLogin] = useState(true);
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const{userData,setUserData} = useContext(AuthContext);
+
+    const userInfo = {
+        "name":name,
+        "email":email,
+        "password":password
+    }
+
+    const setUserInfo = async()=>{
+        setUserData(userInfo);
+        await Services.setUserAuth(userInfo);
+    }
 
     return (
-        <div style={{ height: "100vh", width: "100%", display:"flex", justifyContent:"center",alignItems:"center" }}>
+        <div className='loginScreenBackgroundImg' style={{ height: "100vh", width: "100%", display:"flex", justifyContent:"center",alignItems:"center" }}>
             <div className='card' style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column", width:"25%"}}>
                 <h2 style={{paddingBottom:"1rem"}}>{login?"Login":"Sign Up"}</h2>
                 <Box
@@ -15,13 +33,13 @@ export default function LoginSignupPage() {
                     sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
                     noValidate
                     autoComplete="off" style={{ width:"100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                    {login?<></>:<TextField style={{width:"100%"}} id="outlined-basic" label="Name" variant="outlined"/>}
-                    <TextField style={{width:"100%"}} id="outlined-basic" label="Email" variant="outlined" />
-                    <TextField style={{width:"100%"}} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
+                    {login?<></>:<TextField value={name} onChange={(e)=> setName(e.target.value)} style={{width:"100%"}} id="outlined-basic" label="Name" variant="outlined"/>}
+                    <TextField value={email} onChange={(e)=> setEmail(e.target.value)} style={{width:"100%"}} id="outlined-basic" label="Email" variant="outlined" />
+                    <TextField value={password} onChange={(e)=> setPassword(e.target.value)} style={{width:"100%"}} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
                 </Box>
                 <p onClick={()=> login?setLogin(false):setLogin(true)} style={{ fontSize: ".8rem", cursor: "pointer",paddingBottom:"1rem",paddingTop:"0.8rem" }}>{login?"Don't have an Account":"Already have an Account"}</p>
                 {login?<p style={{ fontSize: ".8rem", cursor: "pointer",paddingBottom:"1rem" }}>Forgot Password</p>:<></>}
-                <Button variant="contained" size="medium">
+                <Button onClick={setUserInfo} variant="contained" size="medium">
                     {login?"Login":"Sign Up"}
                 </Button>
             </div>
